c2f7eac94cacc0aecee1de0a87e79780656ae52d
kkmm1234 <keithmoffitt2001@gmail.com>
2025-03-25 13:19:10 +0000

    SCRUM-27 - Created endpoints for genearting a summary aswell as mcq from ai using RAG

--- routes/query_routes.py
+++ routes/query_routes.py
@@ -5,8 +5,20 @@ from app.query import *
 
 query_bp = Blueprint("query_bp", __name__)
 
-#route to handle pdf uploads
+#route to Q&A queries
 @query_bp.route("/query", methods=["POST"])
 @jwt_required()
 def query_route():
     return query()
+
+#route to handle summary queries
+@query_bp.route("/summary", methods=["POST"])
+@jwt_required()
+def summary_route():
+    return summary()
+
+#route to handle mcq queries
+@query_bp.route("/mcq", methods=["POST"])
+@jwt_required()
+def mcq_route():
+    return mcq()
